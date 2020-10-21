163edda60a267060d5224a86011917e910653a45
thevictimkill <pedrof.ortega@alumnos.udg.mx>
2020-10-21 02:12:56 +0000

    Actividad

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func burbuja(Lista []int){
+ var auxiliar int
+ for i := 0; i < len(Lista); i++ {
+  for j := 0; j < len(Lista); j++ {
+   if Lista[i] < Lista[j] {
+    auxiliar = Lista[i]
+    Lista[i] = Lista[j]
+    Lista[j] = auxiliar
+    }
+  }
+}
+}
+
+func main() {
+  lista := []int{1,6,3,5,4,9,12,34}
+  lista_slice := lista[0:6]
+  burbuja(lista_slice)
 }
